3cc60a922c26d27541f712bb45f143261688f01f
GitHub Action <action@github.com>
2022-08-05 05:49:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/trzhackalawi/zhackyideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.trzhackalawi.zhackyideaplugin.services
+
+import com.github.trzhackalawi.zhackyideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/trzhackalawi/zhackyideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/trzhackalawi/zhackyideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.trzhackalawi.zhackyideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.trzhackalawi.zhackyideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
